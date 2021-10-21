a403530038458c1de76a8b91c4f68c55def84c39
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-21 09:06:29 +0600

    button,spinner done

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,15 +3,11 @@ import React from 'react';
 const Card = (props) => {
  const{name,img,description}=props.item
  return (
- <div className="col">
-    <div className="card">
-      <img src={img}className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className>{name}</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-      </div>
-    </div>
-  </div>
+
+  //bootstrap demo
+ <div>
+  
+ </div>
  );
 };
 
